1fc099dcb814c47dd57ac58b4928efab0c8d4cae
Twisaam Roy <84868926+twisaam-roy@users.noreply.github.com>
2021-07-05 22:56:21 +0530

    Complete Application

---
+++ src/Components/About.tsx
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom';
+
+const About = () => {
+    return (
+        <div>
+            <h4> version: 69</h4>
+            <Link to = "/">Go Back</Link>
+        </div>
+    )
+}
+
+export default About
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,23 +1,24 @@
 import Button from './Button';
+import { useLocation } from 'react-router-dom';
 
 
 function Header(
-    {
-        title,
-        onAdd,
-        showAdd
-    }: any
+  {
+    title,
+    onAdd,
+    showAdd
+  }: any
 ) {
-
-//   const onclick = () => {
-//     console.log('Click');
-// };
+  let location = useLocation()
+  //   const onclick = () => {
+  //     console.log('Click');
+  // };
   return (
-      <header className = 'header'>
-        <h1>{title}</h1>
-        <Button color = {showAdd? 'red':'green'} text = {showAdd? 'Close': 'Add'} onClick = {onAdd} />
-       
-      </header>
+    <header className='header'>
+      <h1>{title}</h1>
+      {location.pathname === "/" && (<Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />)}
+
+    </header>
   );
 
 }
